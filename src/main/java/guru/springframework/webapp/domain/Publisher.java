a5108a9e85ada39747d2794d02bcc353415db253
Mateusz Zielinski <matzielinski13@gmail.com>
2024-04-14 16:06:33 +0200

    Publisher pojo class implemented

---
+++ src/main/java/guru/springframework/webapp/domain/Publisher.java
@@ -0,0 +1,106 @@
+package guru.springframework.webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String adressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Publisher() {
+    }
+
+    public Publisher(Long id, String name, String adressLine1, String city, String state, String zip) {
+        this.id = id;
+        this.name = name;
+        this.adressLine1 = adressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdressLine1() {
+        return adressLine1;
+    }
+
+    public void setAdressLine1(String adressLine1) {
+        this.adressLine1 = adressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", adressLine1='" + adressLine1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publisher publisher)) return false;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
