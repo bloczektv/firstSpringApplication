5d441d32527178a529c3e851a4c72f4ed3d0f87d
Mateusz Zielinski <matzielinski13@gmail.com>
2024-04-14 16:20:41 +0200

    Publisher relationship to books implemented

--- src/main/java/guru/springframework/webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/webapp/domain/Publisher.java
@@ -1,11 +1,10 @@
 package guru.springframework.webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -18,6 +17,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zip;
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
 
     public Publisher() {
     }
@@ -29,6 +31,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
